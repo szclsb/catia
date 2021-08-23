2012d126965e41454f154bd4b8a6d94f8a4cf120
Claudio <szclsb@gmail.com>
2021-08-23 11:44:57 +0200

    Added Catia tlbs

---
+++ shared/Bootstrap.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Runtime.InteropServices;
+using INFITF;
+
+namespace shared
+{
+    public class Bootstrap
+    {
+        private const string ProgramId = "CATIA.Application";
+
+        public static Application Init()
+        {
+            try
+            {
+                return (Application) Marshal.GetActiveObject(ProgramId);
+            }
+            catch
+            {
+                throw new COMException("CATIA is not running.");
+            }
+        }
+
+        public static bool IsDocValid(Document document, params DocType[] docTypes)
+        {
+            return docTypes.Any(docType => docType.IsValid(document));
+        }
+    }
+}
---
+++ shared/DocType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DRAFTINGITF;
+using INFITF;
+using MECMOD;
+using ProductStructureTypeLib;
+
+namespace shared
+{
+    public class DocType
+    {
+        public static DocType Part = new DocType(typeof(PartDocument));
+        public static DocType Product = new DocType(typeof(ProductDocument));
+        public static DocType Drawing = new DocType(typeof(DrawingDocument));
+
+        public readonly Type Type;
+
+        private DocType(Type type)
+        {
+            Type = type;
+        }
+
+        public bool IsValid(Document document)
+        {
+            return document != null && Type.IsInstanceOfType(document);
+        }
+    }
+}
